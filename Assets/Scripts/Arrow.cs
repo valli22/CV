51fd7c5e829e80ea6290d6169cbc357e4fd380ca
valli22 <davidvacasmiguel@hotmail.com>
2018-05-29 03:50:26 +0200

    Sistema de movimiento por menu

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Arrow : MonoBehaviour {
+
+	[SerializeField]
+	Sprite imageNormal;
+	[SerializeField]
+	Sprite imageOnMouse;
+
+	Image image;
+
+	// Use this for initialization
+	void Start () {
+		image = GetComponent<Image> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void MouseEnter(){
+		image.sprite = imageOnMouse;
+	}
+
+	public void MouseExit(){
+		image.sprite = imageNormal;
+	}
+}
---
+++ Assets/Scripts/GM.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GM : MonoBehaviour {
+
+	public static GM instance;
+
+	int state = 0;
+	[SerializeField]
+	GameObject[] uiGO;
+	[SerializeField]
+	Button[] uiButtons;
+
+	public Color pressedColor;
+	public Color unPressedColor;
+	public Color unPressedColorHighlighted;
+
+	// Use this for initialization
+	void Start () {
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void GoTo(int stateToGo){
+		uiGO[state].SetActive (false);
+		ColorBlock cb = uiButtons [state].colors;
+		cb.normalColor = unPressedColor;
+		cb.highlightedColor = unPressedColorHighlighted;
+		uiButtons [state].colors = cb;
+		state = stateToGo;
+		uiGO [state].SetActive (true);
+		cb = uiButtons [state].colors;
+		cb.normalColor = pressedColor;
+		cb.highlightedColor = pressedColor;
+		uiButtons [state].colors = cb;
+	}
+
+	public void PreviousState(){
+		if (state == 0)
+			return;
+
+		GoTo (state-1);
+	}
+
+	public void NextState(){
+		if (state == 4)
+			return;
+
+		GoTo (state+1);
+	}	
+}
