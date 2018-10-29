6ce76bcc132ebf237934acf8f7a3cbb29cf49a50
valli22 <davidvacasmiguel@hotmail.com>
2018-10-29 14:22:13 +0100

    Experiencia profesional Done

---
+++ Assets/Scripts/Game5/Card.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour {
+
+	public Sprite image;
+	public int id;
+
+	float timeWait = 0.8f;
+	Image imageRenderer;
+
+	// Use this for initialization
+	void Start () {
+		imageRenderer = GetComponent<Image> ();
+		if (imageRenderer == null)
+			Debug.LogError ("Image not founded!");
+		imageRenderer.sprite = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Button_Activate_Image(){
+		if (LevelManagerExpLab.instance.canClick == false)
+			return;
+		
+		imageRenderer.sprite = image;
+		LevelManagerExpLab.instance.canClick = false;
+		LevelManagerExpLab.instance.clickCounter++;
+		if (LevelManagerExpLab.instance.clickCounter % 2 == 0)
+			Invoke ("GrantInfoLevelManager", timeWait);
+		else
+			GrantInfoLevelManager ();
+
+	}
+
+	public void DeactivateImage(){
+		imageRenderer.sprite = null;
+	}
+
+	void GrantInfoLevelManager(){
+		LevelManagerExpLab.instance.CardClicked (this.gameObject,id);
+	}
+}
---
+++ Assets/Scripts/Game5/LevelManagerExpLab.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManagerExpLab : MonoBehaviour {
+
+	GameObject goClicked;
+	int idClicked;
+
+	int aciertos = 0;
+
+	public static LevelManagerExpLab instance;
+
+	public bool canClick = true;
+	public int clickCounter = 0;
+
+	public GameObject winPanel;
+
+	// Use this for initialization
+	void Start () {
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void CardClicked(GameObject goIn, int idIn){
+
+		if (goClicked == null) {
+			goClicked = goIn;
+			idClicked = idIn;
+		} else {
+		
+			if (idClicked == idIn) {
+				goClicked.SetActive (false);
+				goIn.SetActive (false);
+
+				aciertos++;
+				if (aciertos >= 4) {
+					winPanel.SetActive (true);
+				}
+			} else {
+				goClicked.GetComponent<Card> ().DeactivateImage ();
+				goIn.GetComponent<Card> ().DeactivateImage ();
+			}
+			goClicked = null;
+			idClicked = -1;
+		}
+
+		canClick = true;
+
+	}
+}
